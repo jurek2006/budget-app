3278fffe2513cdcfaecb315f7f5395c02bb18025
Jurek <jurek2006@gmail.com>
2018-10-12 10:32:16 +0200

    Dodanie bootstrap (reactstrap) i fontawesome

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Collapse, Button, Card, CardBody } from "reactstrap";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = { collapse: false };
+    }
+
+    toggle() {
+        this.setState({ collapse: !this.state.collapse });
+    }
+
     render() {
         return (
             <div className="App">
