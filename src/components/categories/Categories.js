96598ba942940a90e25474ee29822fc0a0730280
Jurek <jurek2006@gmail.com>
2018-10-18 18:18:31 +0200

    Wstępne przygotowanie komponentu kategorii

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -2,11 +2,37 @@ import React, { Component } from "react";
 
 export class Categories extends Component {
     render() {
-        return (
-            <div>
-                <h1>Categories</h1>
-            </div>
-        );
+        const categories = [
+            {
+                id: "445635",
+                name: "jedzenie"
+            },
+            {
+                id: "3546547",
+                name: "rozrywka"
+            },
+            {
+                id: "34457567",
+                name: "opłaty"
+            }
+        ];
+
+        if (categories) {
+            return (
+                <div className="card m-2">
+                    <h1 className="card-header ">Kategorie</h1>
+                    <ul className="list-group list-group-flush">
+                        {categories.map(category => (
+                            <li className="list-group-item" key={category.id}>
+                                {category.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        } else {
+            return <h1>Loading...</h1>;
+        }
     }
 }
 
